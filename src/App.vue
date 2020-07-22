39078bfd0a07cd923478775bc921e32912641fac
jiwoo <dlwldn1346@gmail.com>
2020-07-22 19:30:34 +0900

    second vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
   <div id="app">
-    <h1>안녕 ~!</h1>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Vue 테스트" />
+    <Topbar v-bind:msg="msg" />
+    {{msg}}
+    <br />
+    제이름은: {{name}}
+    <br />
+    <br />
+    <input type="text" v-model="name" />
+    <label>제목</label>
+    <input type="text" v-model="msg" />
+    <Bottom />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Topbar from "./components/Topbar.vue";
+import Bottom from "./components/Bottom.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Topbar,
+    Bottom
+  },
+  data() {
+    return {
+      msg: "Hello Vue JS",
+      name: ""
+    };
   }
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  width: 100%;
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #0066ff;
+}
+
+input {
+  width: 200px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid gray;
+}
+
+.header {
+  background-color: crimson;
+  height: 100px;
+  color: white;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-size: 3rem;
+  margin-bottom: 30px;
+}
+
+.footer {
+  background-color: #333;
+  height: 100px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  margin-top: 30px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 }
 </style>
---
+++ src/components/Bottom.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">Korea IT</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
